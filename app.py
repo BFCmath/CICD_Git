79f3e0539f77048a44233bbf2eb966b8e5e49cc1
BFCmath <23120026@student.hcmus.edu.vn>
2025-12-19 09:25:18 +0700

    wrong commit

--- app.py
+++ app.py
@@ -20,9 +20,8 @@ def multiply(a, b):
 
 def divide(a, b):
     """Divide two numbers"""
-    if b == 0:
-        raise ValueError("Cannot divide by zero")
-    
+    # if b == 0:
+    #     raise ValueError("Cannot divide by zero")
     return a / b
 
 
